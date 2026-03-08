da53d081586ae980ae996c8759270863e90abcb1
Maritetta Katsarou <your-email@github.com>
2026-03-08 13:07:14 +0200

    Site Update

--- src/App.jsx
+++ src/App.jsx
@@ -301,7 +301,7 @@ const ProjectDetail = () => {
 const LogoSVG = () => (
   <svg 
     viewBox="0 0 498 498" 
-    className="w-64 h-64 md:w-80 md:h-80 fill-slate-100 transition-all duration-500 ease-out 
+    className="w-64 h-64 md:w-80 md:h-80 fill-slate-900 transition-all duration-500 ease-out 
                group-hover:drop-shadow group-hover:fill-slate-900"
     xmlns="http://www.w3.org/2000/svg"
   >
